ef90664119faaff4f0c64e4877923df82908f1ef
Loaded-Dice <33704379+Loaded-Dice@users.noreply.github.com>
2023-04-29 16:05:25 +0200

    Improved documentation

--- Nightstick-Arduino/Nightstick-Arduino.ino
+++ Nightstick-Arduino/Nightstick-Arduino.ino
@@ -2,8 +2,21 @@
   IDE used Arduino 1.8.19
   Board: Seeed Studio XIAO nRF52840 BLE Sense
   Core: Seeed Studio nRF52 Boards v1.1.1 (Not the mbed -enabled core!)
-  
+
+  Board specs:
+  Procesor: Nordic nRF52840 - ARM® Cortex®-M4 with FPU run up to 64 MHz
+  Wireless:     Bluetooth 5.0/NFC/Zigbee
+  Flash:  1 MB flash and 256 kB RAM
+  Ext. flash  2 MB QSPI flash
   Board RAM size 256 kB
+  Interface / Pins     1xUART, 1xIIC, 1xSPI, 1xNFC, 1xSWD / (11xGPIO(PWM), 6xADC)
+  Circuit operating voltage: 3.3V@200mA
+  Charging current: 50mA/100mA (internal) / 1A external
+  Input voltage (VIN): 5V
+  Standby power consumption:  <5μA
+
+
+  
   Button voltage divider level is max 3v3 regulated (not battery voltage) 
   
   Can't programm the XIAO nRf52?
@@ -44,7 +57,6 @@ The Nightstick code is distributed in the hope that it will be useful, but WITHO
 You should have received a copy of the GNU Lesser General Public License along with this code.  If not, see [this](https://www.gnu.org/licenses/)
 
 
-
 To do:
 
   New Featues to implement:
@@ -81,40 +93,76 @@ To do:
     - when bmp file & folder --> "-" or "" i n config file (no file or folder selected) load 1st .bmp from first folder when bmp mode is activated.
     - implemet deep sleep for µC or idle animation when not played (make this selectable in config )
 
+    - test lowest voltage where leds are still working
+      consider using charge / discharge board instead of charge only
 
-
-next version main PCB:
-
-  - add button resisor array onto the board.
-  - add circuit to switch LEDs completly off (no standby current) 
-    (maybe other battery charging board? --> charge/discharge with enable function?)
-  - pushbutton on off switch circuit
-  - add (electrolytic) capacitor to main pcb
-  - make a mini push button pcb (curved / buttons pointing down)
-
-// 3D case design improvements
-  - improved battery holder
+  next version main PCB:
+  
+    - add button resisor array onto the board.
+    - add circuit to switch LEDs completly off (no standby current) 
+      (maybe other battery charging board? --> charge/discharge with enable function?)
+    - pushbutton on off switch circuit
+    - add (electrolytic) capacitor to main pcb
+    - make a mini push button pcb (curved / buttons pointing down)
+    - put a connector terminal on the bottom - like ZH 1.5 mm (4 & 6 pin; pins) (90 degree angled connector)
+        (Btn1,Btn2,Btn3,Gnd) (VBat,DO_Led,Gnd) (VBat, DO_Led2, Gnd)(switch1,switch2)
+  
+  3D case design improvements
+    - improved battery holder
  
+               -------------------
+ ------=======|  NIGHTSTICK CODE  |=======-------
+               -------------------
+   ---=={ Current Setup for this sketch }==--- 
 
+        Pinout:
+                      ______
+                  ___| USBC |___
+        (A0) D0  |O  |______|  O|  5V
+        (A1) D1  |O|----------|O|  GND
+        (A2) D2  |O|   XIAO   |O|  3V3
+        (A3) D3  |O| nRF52840 |O|  D10 (A10) MOSI
+    SDA (A4) D4  |O|   BLE    |O|  D9  (A9)  MISO
+    SCL (A5) D5  |O|  SENSE   |O|  D8  (A8)  SCK
+    Tx  (A6) D6  |O|__________|O|  D7  (A7)  Rx
+                  --------------
+
+       
+      |------|------|------|--------------|
+      | ⁪Pin  | GPIO | Pin  | Function     |
+      |------|------|------|--------------|
+      | D0   |  2   ⁪| A0   | LED Data  ⁪   |
+      | D1   ⁪|  3   ⁪| A1   | SD Card CS   |
+      | D2   ⁪|  4   ⁪| A2   | Buttons In   |
+      | D3   ⁪|  5   ⁪| A3   |              |
+      | D4   ⁪|  6   ⁪| SDA  |              |
+      | D5   ⁪|  7   ⁪| SCL  |              |
+      | D6   ⁪| 21   ⁪| Tx   |              |
+      | --   ⁪| --   ⁪| ---- | ------------ |
+      | D10  ⁪| 10   ⁪| MOSI | SD Card      |
+      | D9   ⁪|  9   ⁪| MISO | SD Card      |
+      | D8   ⁪|  8   ⁪| SCK  | SD Card      |
+      | D7   ⁪| 20   ⁪| Rx   |              |
+      |------|------|------|--------------|
+
+                  
+   Main thoughts on the code structure:
+   
+     includes, definitions, Global variables & structs  are in the header file "Nightstick.h"
+     Written in that order for each tab page section separated
+     each tab gets its own setup function on the beginning of the page named setup_tabName() 
+     and if routines need to run within the loop  i created a main_tabName() function right below the setup_tabName() function
+     when a specific timing is needed for the main_ functions, a template function from the fastLED library is used (EVERY_N_MILLIS(n){})
+     with the delay time defined in Nightstick.h
+     
 */
-// Main thoughts on the code structure:
-// includes, definitions, Global variables & structs  are in the header file "Nightstick.h"
-// Written in that order for each tab page section separated
-// each tab gets its own setup function on the beginning of the page named setup_tabName() 
-// and if routines need to run within the loop  i created a main_tabName() function right below the setup_tabName() function
-// when a specific timing is needed for the main_ functions, a template function from the fastLED library is used (EVERY_N_MILLIS(n){})
-// with the delay time defined in Nightstick.h
-// 
-//
+
 #include "Nightstick.h"
 //--------------------------------------------------------------------------------------------------------------------- Setup
 void setup(){ 
-  //do not use Serial.print() or Serial.println()
-  //instead use msg() or msgln() to implement a debug on/off message mode later
+  //do not use Serial.print() or Serial.println() instead use msg() or msgln() to implement a debug on/off message mode later
   Serial.begin(115200);
 
-
-  
   setup_System(); // begin with system setup to enable error blink codes with the internal board leds
   setup_SD();     // boot sequence stops when SD card is removed (safety reason)
   setup_Config(); // find and load config.csv from SD card or write new if not found
@@ -124,10 +172,7 @@ void setup(){
   setup_BLE_COM();
   setup_LEDs();
 
-  
-  // Filling  the buffer to see at compile time what space is used and whats left
-  // can be removed later
-  memset(pixelBuff,0,sizeof(pixelBuff)); 
+  memset(pixelBuff,0,sizeof(pixelBuff));  // Filling  the buffer to see at compile time what space is used and whats left - can be removed later
 }
 
 void loop(){ // all main functions have timining structures integrated
